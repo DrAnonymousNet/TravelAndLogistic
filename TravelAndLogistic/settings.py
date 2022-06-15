e1c566107810fe4ff473b996a2c44da5c67982fc
Mustapha Ahmad <64500446+DrAnonymousNet@users.noreply.github.com>
2022-06-15 10:25:00 -0400

    Update settings.py

--- TravelAndLogistic/settings.py
+++ TravelAndLogistic/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = os.environ("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
